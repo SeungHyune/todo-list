b8e1a641d2c51e34ffcc825f2fc7814ea28fe11e
seunghyune <114329713+SeungHyune@users.noreply.github.com>
2023-07-01 18:00:46 +0900

    todo-list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input[type='text']");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.getElementById("todo-list");
 const greeting = document.getElementById("greeting");
+const logoutBtn = document.querySelector(".header button");
+const warningCloseBtn = document.querySelector(".warning button");
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = "username";
@@ -8,23 +12,50 @@ const USERNAME_KEY = "username";
 
 function loginOnSubmit(e){
     e.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-
     const userName = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, userName);
+    const pattern = /^\s+|\s+$/g;
+    if(userName.replace(pattern, '' ) == "" || userName === ""){
+        warning.classList.add('show');
+    } else {
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        todoList.classList.remove(HIDDEN_CLASSNAME);
 
-    paintGreetings(userName);
+        loginInput.value = "";
+        localStorage.setItem(USERNAME_KEY, userName);
+        paintGreetings(userName);
+    }
+
+    warningCloseBtn.addEventListener('click', () => {
+        warning.classList.remove('show');
+    })
+}
+
+function logoutSubmit(){
+    todoForm.style.opacity = 0;
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    todoList.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = ``;
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    logoutBtn.classList.add(HIDDEN_CLASSNAME);
+
+    localStorage.removeItem(USERNAME_KEY);
 }
 
 function paintGreetings(username){
     greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.classList.remove(HIDDEN_CLASSNAME);
+    todoForm.style.opacity = 1;
+
+    logoutBtn.addEventListener('click', logoutSubmit);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    todoList.classList.add(HIDDEN_CLASSNAME);
     loginForm.addEventListener('submit', loginOnSubmit);
 } else {
     paintGreetings(savedUsername);
 }
+
--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,9 @@
-const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
-const todoList = document.getElementById("todo-list");
+const warning = document.querySelector(".warning");
 
 let todos = [];
-const TODO_KEY = 'todos'
+const TODO_KEY = 'todos';
+const CHECK_KEY = 'check';
 
 function saveTodos(){
     localStorage.setItem(TODO_KEY, JSON.stringify(todos));
@@ -13,6 +13,33 @@ function todoDelete(e){
     const li = e.target.parentElement;
     li.remove();
     todos = todos.filter(todo => todo.id !== Number(li.id));
+    saveTodos();
+}
+
+function strikethrough(e){
+    const li = e.target.parentElement;
+    const span = e.target;
+    let idx;
+    let obj = {};
+    const todoLi = todos.filter((todo,i) => {
+        if(todo.id === Number(li.id)){
+            idx = i;
+            obj.text = todo.text;
+            obj.id = todo.id;
+            obj.check = todo.check;
+            return todo.id === Number(li.id);
+        }
+    });
+    
+    if(!obj.check){
+        span.classList.add(CHECK_KEY);
+        obj.check = true;
+        todos[idx] = obj;
+    } else {
+        span.classList.remove(CHECK_KEY);
+        obj.check = false;
+        todos[idx] = obj;
+    }
     saveTodos();
 }
 
@@ -27,18 +54,32 @@ function paintTodo(newTodo){
     li.appendChild(button);
     todoList.appendChild(li);
 
+    if(newTodo.check){
+        span.classList.add(CHECK_KEY);
+    }
+
     button.addEventListener('click', todoDelete);
+    span.addEventListener('click', strikethrough);
 }
 
 function handleTodoSubmit(e){
     e.preventDefault();
     const newTodo = todoInput.value;
-    const newTodoObj = {text:newTodo,id:Date.now()};
-    todos.push(newTodoObj);
-    todoInput.value = "";
-    paintTodo(newTodoObj);
+    const pattern = /^\s+|\s+$/g;
+    if(newTodo.replace(pattern, '' ) == "" || newTodo === ""){
+        warning.classList.add('show');
+    } else {
+        const newTodoObj = {text:newTodo,id:Date.now(),check:false};
+        todos.push(newTodoObj);
+        todoInput.value = "";
+        paintTodo(newTodoObj);
 
-    saveTodos();
+        saveTodos();
+    }
+
+    warningCloseBtn.addEventListener('click', () => {
+        warning.classList.remove('show');
+    })
 }
 
 todoForm.addEventListener('submit', handleTodoSubmit);
